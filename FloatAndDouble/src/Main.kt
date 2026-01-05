3d4c982891888a672cb0e71c9380ff67f41647dd
kkamara <kamaracomputers@gmail.com>
2026-01-05 06:55:04 +0000

    Add project: FloatAndDouble

---
+++ FloatAndDouble/src/Main.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val myNumber = 2F // Outputs 2.0
+//    val mySecondNumber: Double = 2 Error
+    val mySecondNumber = 2.0
+
+    println("myNumber = $myNumber and mySecondNumber = $mySecondNumber")
+}
