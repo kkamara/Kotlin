794a09c528b911236ba83ecb37e8e73ad6ac4c5d
kkamara <kamaracomputers@gmail.com>
2026-01-05 08:55:57 +0000

    Add more work to Operators project

--- Operators/src/Main.kt
+++ Operators/src/Main.kt
@@ -42,7 +42,7 @@ fun main() {
     println("--x = ${--x}")
     println("x = $x")
 
-    val isActive = false
+    var isActive = false
     if(true == isActive) { // Personal note: this is Yoda Notation - a standard I follow.
         println("The condition is true.")
     } else {
@@ -63,5 +63,40 @@ fun main() {
         println("The number is greater than 4.")
     } else {
         println("The condition is false.")
+    }
+
+    myNumber = 150
+    if (150 > myNumber) {
+        println("Less than 150.")
+    } else if (150 >= myNumber) {
+        println("Less than 150 or equal to 150.")
+    } else {
+        println("The condition failed.")
+    }
+
+    if (150 != myNumber) {
+        println("Not equal to 150.")
+    }
+
+    myNumber = 100
+    if (150 != myNumber) {
+        println("Not equal to 150.")
+    }
+
+    isActive = false
+    if (!isActive) {
+        println("The user is not active.")
+    } else {
+        println("The user is active.")
+    }
+
+    isActive = true
+    val score = 100
+    val internetSpeed = 5000
+
+    if (isActive && 100 == score && 5000 == internetSpeed) {
+        println("You are at the next level.")
+    } else {
+        println("You are at the same level.")
     }
 }
