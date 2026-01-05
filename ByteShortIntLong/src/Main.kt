f5b1dcc0fa1598730ef3b58ac998ddf6369c45c1
kkamara <kamaracomputers@gmail.com>
2026-01-05 06:44:16 +0000

    Add project: ByteShortIntLong

---
+++ ByteShortIntLong/src/Main.kt
@@ -0,0 +1,27 @@
+fun main() {
+    var number = 29
+
+    val integerMaxValue = Int.MAX_VALUE
+    val integerMinValue = Int.MIN_VALUE
+    println("Integer max value: $integerMaxValue")
+    println("Integer min value: $integerMinValue")
+
+    number = 2147483647
+
+    val byteMaxValue = Byte.MAX_VALUE
+    val byteMinValue = Byte.MIN_VALUE
+    println("Byte max value: $byteMaxValue")
+    println("Byte min value: $byteMinValue")
+
+    val shortMaxValue = Short.MAX_VALUE
+    val shortMinValue = Short.MIN_VALUE
+    println("Short max value: $shortMaxValue")
+    println("Short min value: $shortMinValue")
+
+    val longMaxValue = Long.MAX_VALUE
+    val longMinValue = Long.MIN_VALUE
+    println("Long max value: $longMaxValue")
+    println("Long min value: $longMinValue")
+
+    val myNumber = 28L
+}
