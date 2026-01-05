a5b4d7e0e933991c6fb729e887b0ce0beb6ae640
kkamara <kamaracomputers@gmail.com>
2026-01-05 06:33:00 +0000

    Add project: IntegerType

---
+++ IntegerType/src/Main.kt
@@ -0,0 +1,10 @@
+fun main() {
+    var number = 29
+
+    val integerMaxValue = Int.MAX_VALUE
+    val integerMinValue = Int.MIN_VALUE
+    println("Integer max value: $integerMaxValue")
+    println("Integer min value: $integerMinValue")
+
+    number = 2147483647
+}
