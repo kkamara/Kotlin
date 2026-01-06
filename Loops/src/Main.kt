21fa3e1e5f59089a233214df96a84a76ee17fd51
kkamara <kamaracomputers@gmail.com>
2026-01-06 14:32:46 +0000

    Add more work to Loops project

--- Loops/src/Main.kt
+++ Loops/src/Main.kt
@@ -11,7 +11,51 @@ fun main() {
 //    for (i in 1 until 10 step 2) {
 //        println(i)
 //    }
-    for (i in 1..10) {
-        println(i)
+//    for (i in 1..10) {
+//        println(i)
+//    }
+    var number = 0
+//    while (number < 10) {
+//        println(number)
+//        number++
+//    }
+//    while (number < 10) println(number++)
+//    do {
+//        println(number++)
+//    } while (number < 10)
+//    while (number < 10) {
+//        if (7 == number) {
+//            continue
+//        }
+//        println(number)
+//        number++
+//    }
+//    while (number < 10) {
+//        number++
+////        if (2 < number && 8 > number) {
+//        if (number in 3..7) {
+//            continue
+////            break
+//        }
+//        println(number)
+//    }
+//    for (i in 0..10) {
+////        if (i in 3..8) {
+//        if (7 == i) {
+////            continue
+//            break
+//        }
+//        println(i)
+//    }
+    outer@ while (5 > number) {
+        number++
+        println(number)
+//        Cannot target an inner loop from an outer loop.
+        var i = 0
+        while (5 > i) {
+            if (0 == i) break@outer
+            i++
+            println("***$i")
+        }
     }
 }
