df9a5c2c4c5fc8b81950fdc1331cd764cdfdfa7a
kkamara <kamaracomputers@gmail.com>
2026-01-06 14:03:52 +0000

    Add project: Loops

---
+++ Loops/src/Main.kt
@@ -0,0 +1,17 @@
+fun main() {
+    /*for (i in 1..10) {
+        println(i)
+    }*/
+    /*for (i in 1 until 10) { // Number on the left must be smallers than the number on the right.
+        println(i)
+    }*/
+    /*for (i in 10 downTo 1 step 2) {
+        println(i)
+    }*/
+//    for (i in 1 until 10 step 2) {
+//        println(i)
+//    }
+    for (i in 1..10) {
+        println(i)
+    }
+}
