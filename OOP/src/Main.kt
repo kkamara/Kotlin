cc605aa17ff901432539089db53568f6f533fd72
kkamara <kamaracomputers@gmail.com>
2026-01-07 08:32:03 +0000

    Add more work to OOP project 5

--- OOP/src/Main.kt
+++ OOP/src/Main.kt
@@ -4,18 +4,25 @@ fun main() {
     val friend = User(firstName = "John", lastName = "Smith")
     val user2 = User(age = 19, firstName = "Ioana", lastName = "Dobbi")
 
-    println("FirstName = ${user.firstName}")
-    println("LastName = ${user.lastName}")
-    println("Age = ${user.age}")
+    user.firstName = "Vlad"
+
+    println(user.firstName)
+    println(user.lastName)
+    println(user.age)
 
     println()
 
-    println("FirstName = ${friend.firstName}")
-    println("LastName = ${friend.lastName}")
-    println("Age = ${friend.age}")
+    println(friend.firstName)
+    println(friend.lastName)
+    println(friend.age)
 }
 
-class User(var firstName: String, var lastName: String = "LastName", var age: Int = 0) {
+class User(firstName: String, var lastName: String = "LastName", var age: Int = 0) {
 //    init {}
 //    constructor(firstName: String, lastName: String) : this(firstName, lastName) {}
+    var firstName = firstName
+    get() = field
+    set(value) {
+        field = value
+    }
 }
