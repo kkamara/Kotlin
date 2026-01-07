bcdac601ea0e4424cc0afc3d022131efec7670e3
kkamara <kamaracomputers@gmail.com>
2026-01-07 07:46:36 +0000

    Add more work to OOP project 3

--- OOP/src/Main.kt
+++ OOP/src/Main.kt
@@ -1,50 +1,26 @@
 fun main() {
-    val car1 = Car(" Tesla   ", "S Plaid", "Red", 4)
-//    car1.name = "Tesla"
-//    car1.model = "S Plaid"
-//    car1.color = "Red"
-//    car1.doors = 4
-/*
-    println("Name = ${car1.name}")
-    println("Model = ${car1.model}")
-    println("Color = ${car1.color}")
-    println("Doors = ${car1.doors}")
+    // Using secondary constructor.
+    val user = User("Kel")
+    val friend = User("John", "Smith")
 
-    car1.move()
-    car1.stop()
-
-    val car2 = Car("Ford", "Mustang", "Blue", 2)
+    println("Name = ${user.name}")
+    println("LastName = ${user.lastName}")
+    println("Age = ${user.age}")
 
     println()
-    println("Name = ${car2.name}")
-    println("Model = ${car2.model}")
-    println("Color = ${car2.color}")
-    println("Doors = ${car2.doors}")
 
-    car2.move()
-    car2.stop()
- */
-    val user = User("Kel", "Kamara", 29)
-//    val friend = User("John", "Smith", 30)
-//    val friend = User("Kev", "Smith", 30)
-    val friend = User("John", "Smith", 30)
-
-    println("Name ${friend.name}")
+    println("Name = ${friend.name}")
+    println("LastName = ${friend.lastName}")
+    println("Age = ${friend.age}")
 }
 
-class User(name: String, var lastName: String, var age: Int) {
-    var name: String
-
-    init {
-        if (name.lowercase().startsWith('k')) {
-            this.name = name
-        } else {
-            this.name = "User"
-            println("The name doesn't start with the letter 'k' or 'K'.")
-        }
+class User(var name: String, var lastName: String, var age: Int) {
+    // Secondary constructors cannot declare properties, only params.
+    constructor(name: String): this(name, "LastName", 0) {
+        println("2nd")
     }
 
-    init { // The order of which initialiser blocks are declared matters.
-
+    constructor(name: String, lastName: String) : this(name, lastName, 0) {
+        println("3rd")
     }
 }
