67af8a011fbdc2094906090515132c9dbdeb67e0
kkamara <kamaracomputers@gmail.com>
2026-01-07 08:48:52 +0000

    Add more work to OOP project 6

--- OOP/src/Main.kt
+++ OOP/src/Main.kt
@@ -1,28 +1,10 @@
 fun main() {
-    // Named arguments can only be used with primary constructors.
-    val user = User("Kel")
-    val friend = User(firstName = "John", lastName = "Smith")
-    val user2 = User(age = 19, firstName = "Ioana", lastName = "Dobbi")
+    val user = User("Kel", "Kamara", 29)
 
-    user.firstName = "Vlad"
-
-    println(user.firstName)
-    println(user.lastName)
-    println(user.age)
-
-    println()
-
-    println(friend.firstName)
-    println(friend.lastName)
-    println(friend.age)
+    user.favoriteMovie = "Interstellar"
+    println(user.favoriteMovie)
 }
 
-class User(firstName: String, var lastName: String = "LastName", var age: Int = 0) {
-//    init {}
-//    constructor(firstName: String, lastName: String) : this(firstName, lastName) {}
-    var firstName = firstName
-    get() = field
-    set(value) {
-        field = value
-    }
+class User(var firstName: String, var lastName: String, var age: Int) {
+    lateinit var favoriteMovie: String // lateint only on reference types.
 }
