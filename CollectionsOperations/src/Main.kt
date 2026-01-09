cfb40e37ceca6d8d221f945cba7263fa27e565d5
kkamara <kamaracomputers@gmail.com>
2026-01-09 17:06:01 +0000

    Add more work to CollectionsOperations project 12

--- CollectionsOperations/src/Main.kt
+++ CollectionsOperations/src/Main.kt
@@ -3,26 +3,7 @@ fun main() {
         1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30
     )))
 }
-
+// Elements need to be sorted for Binary Search to work.
 private fun searchElement(
     searchedElement: Int, numbers: MutableList<Int>
-): Int {
-    var low = 0
-    val high = numbers.size - 1
-
-    var i = 0
-    while(low <= high) {
-        i++
-        println("Searched number: $i")
-        val mid = (low + high) / 2
-        val cmp = numbers[mid].compareTo(searchedElement)
-        if (0 > cmp) {
-            low = mid + 1
-        } else if (cmp > 0) {
-            low = mid - 1
-        } else {
-            return numbers[mid]
-        }
-    }
-    return -1
-}
+): Int = numbers[numbers.binarySearch(27)]
