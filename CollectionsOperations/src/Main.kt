eeeb1f37c750e6736df177d00ae83cd9ab204774
kkamara <kamaracomputers@gmail.com>
2026-01-09 14:57:40 +0000

    Add more work to CollectionsOperations project 6

--- CollectionsOperations/src/Main.kt
+++ CollectionsOperations/src/Main.kt
@@ -1,9 +1,27 @@
 fun main() {
-    val numbers = listOf(
-        "one", "two", "three", "four", "five"
+    val numbersStrings = listOf(
+        "one", "two", "three", "four", "five", "six"
     )
-    println(numbers.groupBy { it.first().uppercase() })
-    println(numbers.groupBy(
-        keySelector = { it.first() }, valueTransform = { it.uppercase() })
-    )
+    println(numbersStrings.slice(1..3))
+    println(numbersStrings.slice(0..4 step 2))
+    println(numbersStrings.slice(setOf(3, 5, 0)))
+    print("\n")
+    println(numbersStrings.take(3))
+    println(numbersStrings.takeLast(3))
+    println(numbersStrings.drop(1))
+    println(numbersStrings.dropLast(5))
+    print("\n")
+    println(numbersStrings.takeWhile{ !it.startsWith("f") })
+    println(numbersStrings.takeLastWhile { "three" != it })
+    println(numbersStrings.dropWhile { 3 == it.length })
+    println(numbersStrings.dropLastWhile { it.contains("i") })
+    print("\n")
+
+    val numbers = (0..13).toList()
+    println(numbers.chunked(3))
+    println(numbers.chunked(3) { it.sum() })
+    print("\n")
+
+    val numbersStrings2 = numbersStrings
+    println(numbersStrings2.windowed(3))
 }
