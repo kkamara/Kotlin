2bdef77cd56726a2a4e9e89329fdc569b4a7179c
kkamara <kamaracomputers@gmail.com>
2026-01-12 16:26:04 +0000

    Add project: KotlinCoroutinesSnapshot4Point1

---
+++ KotlinCoroutinesSnapshot4Point1/src/main/kotlin/Main.kt
@@ -0,0 +1,41 @@
+package com.kelvinkamara
+
+import kotlinx.coroutines.*
+
+fun main() { // Executed on the main thread.
+    println("Main program starts: ${Thread.currentThread().name}")
+
+    val start = System.currentTimeMillis()
+    // Parent coroutine.
+    val parentJob = CoroutineScope(Dispatchers.Default).launch {
+        val job1 = withTimeout(3000) { // KotlinCoroutinesSnapshot4Point1.
+            println(getData1(Thread.currentThread().name))
+        }
+        val job2 = launch {
+            println(getData2(Thread.currentThread().name))
+        }
+    }
+    runBlocking { parentJob.join() }
+    parentJob.invokeOnCompletion {
+        it?.let {
+            println("Parent job FAILED: ${it.message}")
+        } ?: println("Parent job SUCCESS!")
+    }
+    println("Total time: ${System.currentTimeMillis() - start}ms")
+
+    println("Main program ends: ${Thread.currentThread().name}")
+}
+
+private suspend fun getData1(threadName: String): String {
+    println("Fake work1 starts: $threadName")
+    delay(2000)
+    println("Fake work1 finished: $threadName")
+    return "Result 1"
+}
+
+private suspend fun getData2(threadName: String): String {
+    println("Fake work2 starts: $threadName")
+    delay(2000)
+    println("Fake work2 finished: $threadName")
+    return "Result 2"
+}
