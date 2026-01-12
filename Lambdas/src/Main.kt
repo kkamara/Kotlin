e522c11db3e36506a9e2d4238bcf1db0eda468a5
kkamara <kamaracomputers@gmail.com>
2026-01-12 11:51:35 +0000

    Add more work to Lambdas project

--- Lambdas/src/Main.kt
+++ Lambdas/src/Main.kt
@@ -5,8 +5,28 @@ fun main() {
     If a function can do both, that is also called a Higher Order Function.
     A lambda is a function with no name.
  */
-//    val myLambda = {a: Int -> println(a)}
-    add(5, 3, {a: Int -> println(a)})
+    //    val myLambda = {a: Int -> println(a)}
+//    add(5, 3, { a: Int -> println(a) }, 2)
+    add(5, 3) { a: Int -> println(a) }
+
+    val loginButton = Button("Login", 34345) {
+//      Login user.
+        println("test")
+    }
+
+    val signUpButton = Button("Sign Up", 2345) {
+//      Sign up the user.
+    }
+}
+
+class Button(val text: String, val id: Int, val onClickListener: () -> Unit) {
+    init{
+        onClickListener()
+    }
+}
+
+fun add(a: Int, b: Int, action: (Int) -> Unit, c: Int) {
+    action(a + b)
 }
 
 fun add(a: Int, b: Int, action: (Int) -> Unit) {
