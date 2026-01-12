4ad1e099ba0ec986cf81905f26566ecd0d36d71c
kkamara <kamaracomputers@gmail.com>
2026-01-12 11:58:55 +0000

    Add project: LambdasSnapshot3

---
+++ LambdasSnapshot3/src/Main.kt
@@ -0,0 +1,42 @@
+fun main() {
+    /*
+        If a function can accept a function as a parameter that is called a Higher Order Function.
+        If a function can return a function that is also called a Higher Order Function.
+        If a function can do both, that is also called a Higher Order Function.
+        A lambda is a function with no name.
+     */
+    //    val myLambda = {a: Int -> println(a)}
+//    add(5, 3, { a: Int -> println(a) }, 2)
+    add(5, 3) { a: Int -> println(a) }
+
+    val loginButton = Button("Login", 34345) {
+//      Login user.
+        println("test")
+    }
+
+    val signUpButton = Button("Sign Up", 2345) {
+//      Sign up the user.
+    }
+
+    upperCase("hello") { it.uppercase() }
+}
+
+class Button(val text: String, val id: Int, val onClickListener: () -> Unit) {
+    init {
+        onClickListener()
+    }
+}
+
+fun add(a: Int, b: Int, action: (Int) -> Unit, c: Int) {
+    action(a + b)
+}
+
+fun add(a: Int, b: Int, action: (Int) -> Unit) {
+    action(a + b)
+}
+
+fun upperCase(str: String, myFunction: (String) -> String) {
+    val upperCasedWord = myFunction(str)
+
+    println(upperCasedWord)
+}
