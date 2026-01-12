164047c8c9421af90bd6be32a5e737ff692f8516
kkamara <kamaracomputers@gmail.com>
2026-01-12 08:52:28 +0000

    Add more work to OOP project 21

--- OOP/src/Main.kt
+++ OOP/src/Main.kt
@@ -1,9 +1,9 @@
-class Team<T : Player>(val name: String, private val players: MutableList<in T>) {
+class Team<T : Player>(val name: String, private val players: MutableList<out T>) {
     fun addPlayer(player: T) {
         if (players.contains(player)) {
             println("Player: ${player.name} is already in the team ${this.name}.")
         } else {
-            players.add(player)
+//            players.add(player)
             println("Player: ${player.name} was added to the team ${this.name}.")
         }
     }
@@ -20,14 +20,25 @@ open class GamesPlayer(name: String) : Player(name)
 class CounterStrikePlayer(name: String) : GamesPlayer(name)
 
 fun main() {
-//  Covariance: set line 1 `in` to `out`.
-//    val footballTeam = Team<Player>(
-//        "Football Team",
-//        mutableListOf<FootballPlayer>(FootballPlayer("Player 1"), FootballPlayer("Player 2"))
-//    )
-//  Contravariance
-    val gamesTeam = Team<CounterStrikePlayer>(
-        "Games Team",
-        mutableListOf<GamesPlayer>(GamesPlayer("Player 1"), GamesPlayer("Player 2"))
+    val mixedList = mutableListOf(
+        1, 2, 360, 'a', 'b', 'c', "hello", "world"
     )
+
+    val specificList = getSpecificTypes<Char>(mixedList)
+
+    for (element in specificList) {
+        println(element)
+    }
+}
+
+inline fun<reified T> getSpecificTypes(list: List<Any>): List<T> {
+    val specificList = mutableListOf<T>()
+
+    for (element in list) {
+        if (element is T) {
+            specificList.add(element)
+        }
+    }
+
+    return specificList
 }
