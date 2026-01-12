d2b313f38d75050e2855b182138c5f1129b90f43
kkamara <kamaracomputers@gmail.com>
2026-01-12 17:23:17 +0000

    Add more work to KotlinCoroutines project 5

--- KotlinCoroutines/src/main/kotlin/Main.kt
+++ KotlinCoroutines/src/main/kotlin/Main.kt
@@ -2,19 +2,26 @@ package com.kelvinkamara
 
 import kotlinx.coroutines.*
 
+val handler = CoroutineExceptionHandler { _, exception ->
+    println("Error in one of the children: ${exception.message}")
+}
+
 fun main() { // Executed on the main thread.
     println("Main program starts: ${Thread.currentThread().name}")
 
     val start = System.currentTimeMillis()
     // Parent coroutine.
-    val parentJob = CoroutineScope(Dispatchers.Default).launch {
-        // The following returns null if the timeout is exceeded.
-        val job1 = withTimeoutOrNull(1000) {
-            println(getData1(Thread.currentThread().name))
-        }
-        println(job1)
-        val job2 = launch {
-            println(getData2(Thread.currentThread().name))
+    val parentJob = CoroutineScope(Dispatchers.Default).launch(handler) {
+        supervisorScope {
+            val job1 = launch {
+                println(getData1(Thread.currentThread().name))
+            }
+            val job2 = launch {
+                println(getData2(Thread.currentThread().name))
+            }
+            val job3 = launch {
+                println(getData3(Thread.currentThread().name))
+            }
         }
     }
     runBlocking { parentJob.join() }
@@ -30,6 +37,7 @@ fun main() { // Executed on the main thread.
 
 private suspend fun getData1(threadName: String): String {
     println("Fake work1 starts: $threadName")
+    throw Exception("Error while getting the data in getData1()")
     delay(2000)
     println("Fake work1 finished: $threadName")
     return "Result 1"
@@ -37,7 +45,15 @@ private suspend fun getData1(threadName: String): String {
 
 private suspend fun getData2(threadName: String): String {
     println("Fake work2 starts: $threadName")
+    throw Exception("Error while getting the data in getData2()")
     delay(2000)
     println("Fake work2 finished: $threadName")
     return "Result 2"
+}
+
+private suspend fun getData3(threadName: String): String {
+    println("Fake work3 starts: $threadName")
+    delay(2000)
+    println("Fake work3 finished: $threadName")
+    return "Result 3"
 }
