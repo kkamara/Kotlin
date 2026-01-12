b82c4cd2549af347467366a0a0b9bf73ade37703
kkamara <kamaracomputers@gmail.com>
2026-01-12 07:48:48 +0000

    Add more work to OOP project 19

--- OOP/src/Main.kt
+++ OOP/src/Main.kt
@@ -1,38 +1,35 @@
-import kotlin.properties.ReadWriteProperty
-import kotlin.reflect.KProperty
-
 fun main() {
-    val user = User()
-    with(user) {
-        firstName = "Kel"
-        lastName = "Kamara"
-    }
-    with(user) {
-        println(firstName)
-        println(lastName)
+    val footballPlayer = FootballPlayer("Football Player 1")
+    val footballPlayer2 = FootballPlayer("Football Player 2")
+
+    val baseballPlayer = BaseballPlayer("Baseball Player 1")
+    val baseballPlayer2 = BaseballPlayer("Baseball Player 2")
+//  Implicitly set FootballPlayer type.
+    val footballTeam = Team("Football Team", mutableListOf(footballPlayer))
+    footballTeam.addPlayer(footballPlayer2)
+//    footballTeam.addPlayer(baseballPlayer) // Not the generic type given.
+
+    val baseballTeam = Team("Baseball Team", mutableListOf(baseballPlayer))
+    baseballTeam.addPlayer(baseballPlayer2)
+//  Explicitly set GamesPlayer type.
+    val gamesTeam = Team<GamesPlayer>("Games Team", mutableListOf())
+}
+
+class Team<T: Player>(val name: String, val players: MutableList<T>) {
+    fun addPlayer(player: T) {
+        if (players.contains(player)) {
+            println("Player: ${player.name} is already in the team ${this.name}.")
+        } else {
+            players.add(player)
+            println("Player: ${player.name} was added to the team ${this.name}.")
+        }
     }
 }
 
-class User {
-    var firstName by FormatDelegate()
-    var lastName by FormatDelegate()
-}
+open class Player(val name: String)
 
-class FormatDelegate: ReadWriteProperty<Any?, String> {
-    private var formattedString: String = ""
+class FootballPlayer(name: String) : Player(name)
 
-    override fun getValue(
-        thisRef: Any?,
-        property: KProperty<*>,
-    ): String {
-        return formattedString
-    }
+class BaseballPlayer(name: String) : Player(name)
 
-    override fun setValue(
-        thisRef: Any?,
-        property: KProperty<*>,
-        value: String
-    ) {
-        formattedString = value.lowercase()
-    }
-}
+class GamesPlayer(name: String) : Player(name)
